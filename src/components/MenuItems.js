415d812ff1c6b92584764012db209b2a1f1bef97
Ashish G <ashishgupta8738@gmail.com>
2023-03-14 16:02:22 +0530

    first commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Dropdown from "./Dropdown";
+
+const MenuItems = ({ items, depthLevel }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true);
+  };
+
+  const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false);
+  };
+
+  return (
+    <li
+      className="menu-items"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {items.submenu ? (
+        <>
+          <button
+            type="button"
+            aria-haspopup="menu"
+            aria-expanded={dropdown ? "true" : "false"}
+            onClick={() => setDropdown((prev) => !prev)}
+          >
+            {items.title}
+            {depthLevel > 0 ? (
+              <span> &raquo; </span>
+            ) : (
+              <span className="arrow" />
+            )}
+          </button>
+          <Dropdown
+            depthLevel={depthLevel}
+            submenus={items.submenu}
+            dropdown={dropdown}
+          />
+        </>
+      ) : (
+        <a href="/#"> {items.title} </a>
+      )}
+    </li>
+  );
+};
+
+export default MenuItems;
---
+++ src/menuItems.js
@@ -0,0 +1,46 @@
+export const menuItems = [
+  {
+    title: "Property",
+  },
+  {
+    title: "Fashion",
+    submenu: [
+      {
+        title: "Men",
+        submenu: [
+          {
+            title: "Cloths",
+          },
+          {
+            title: "Jewellary",
+          },
+        ],
+      },
+      {
+        title: "Women",
+        submenu: [
+          {
+            title: "Cloths",
+          },
+          {
+            title: "Jewellary",
+          },
+        ],
+      },
+      {
+        title: "Kids",
+      },
+    ],
+  },
+  {
+    title: "Serivces",
+    submenu: [
+      {
+        title: "Personal & More",
+      },
+      {
+        title: "Appliance Repair",
+      },
+    ],
+  },
+];
