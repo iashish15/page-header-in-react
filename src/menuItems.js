8a3a084768e1996c8fc4055c7a17ca2709d0f101
Ashish G <ashishgupta8738@gmail.com>
2023-03-18 01:02:39 +0530

    changes

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,6 +1,54 @@
 export const menuItems = [
   {
     title: "Property",
+    submenu: [
+      {
+        title: "Residential",
+        submenu: [
+          {
+            title: "Apartment/Flat/Builder Floor",
+          },
+          {
+            title: "Residential Land",
+          },
+          {
+            title: "House/Villa",
+          },
+          {
+            title: "Farm House",
+          },
+          {
+            title: "Other",
+          },
+        ],
+      },
+      {
+        title: "Commercial",
+        submenu: [
+          {
+            title: "Office",
+          },
+          {
+            title: "Retails",
+          },
+          {
+            title: "Commercial Land",
+          },
+          {
+            title: "Industry",
+          },
+          {
+            title: "Storage",
+          },
+          {
+            title: "Hospitality",
+          },
+          {
+            title: "Others",
+          },
+        ],
+      },
+    ],
   },
   {
     title: "Fashion",
@@ -14,6 +62,12 @@ export const menuItems = [
           {
             title: "Jewellary",
           },
+          {
+            title: "Footwear",
+          },
+          {
+            title: "Accessories",
+          },
         ],
       },
       {
@@ -25,10 +79,30 @@ export const menuItems = [
           {
             title: "Jewellary",
           },
+          {
+            title: "Footwear",
+          },
+          {
+            title: "Accessories",
+          },
         ],
       },
       {
         title: "Kids",
+        submenu: [
+          {
+            title: "Cloths",
+          },
+          {
+            title: "Jewellary",
+          },
+          {
+            title: "Footwear",
+          },
+          {
+            title: "Accessories",
+          },
+        ],
       },
     ],
   },
@@ -37,10 +111,88 @@ export const menuItems = [
     submenu: [
       {
         title: "Personal & More",
+        submenu: [
+          {
+            title: "Astrologer",
+          },
+          {
+            title: "Baby Portfolio Photographer",
+          },
+          {
+            title: "Commerce Home Tutor",
+          },
+          {
+            title: "Car Cleaning",
+          },
+          {
+            title: "CCTV Cameras and Installation",
+          },
+          {
+            title: "Divorce Lawyer",
+          },
+          {
+            title: "Home Tutor",
+          },
+          {
+            title: "Dry Cleaning",
+          },
+          {
+            title: "Lawyer",
+          },
+          {
+            title: "Maternity Photographer",
+          },
+          {
+            title: "Packers & Movers",
+          },
+          {
+            title: "Real Estate Lawyer",
+          },
+          {
+            title: "Mathematics Tutor",
+          },
+          {
+            title: "CA for Income Tax Filing",
+          },
+        ],
       },
       {
         title: "Appliance Repair",
       },
     ],
+  },
+  {
+    title: "Furniture",
+    submenu: [
+      {
+        title: "Sofa & Dining",
+      },
+      {
+        title: "Beds & Wardrobes",
+      },
+      {
+        title: "Home Decor & Garden",
+      },
+      {
+        title: "Kids Furniture",
+      },
+      {
+        title: "Other Household Items",
+      },
+    ],
+  },
+  {
+    title: "Car & Bikes",
+    submenu: [
+      {
+        title: "Car",
+      },
+      {
+        title: "Bike",
+      },
+    ],
+  },
+  {
+    title: "Jobs",
   },
 ];
